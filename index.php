b91417d33671ef3576a3d21d7e66e66c2c563471
sunit <sunit.choithani@hnrtech.com>
2022-09-16 22:55:19 +0530

    side nav and header styling

--- index.php
+++ index.php
@@ -11,11 +11,19 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
+    <script src="assets/js/commonActions.js"></script>
 </head>
 <body>
     <div id="pageContainer">
-        <div id="mastHeadContainer">hello</div>
-        <div id="sideNavContainer"></div>
+        <div id="mastHeadContainer">
+            <button class="navShowHide">
+                <img src="assets/images/icons/menu.png" alt="menu">
+            </button>
+            <a href="index.php" class="logoContainer">
+                <img src="assets/images/icons/playTubeLogo.jpg" title="logo" alt="siteLogo">
+            </a>
+        </div>
+        <div id="sideNavContainer" style="display:none"></div>
         <div id="mainSectionContainer">
             <div id="mainContentContainer">hello</div>
         </div>
