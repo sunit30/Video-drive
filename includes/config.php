e08b7d811acfa46bcb5c4f9b1a9b143bbb72a79e
sunit <sunit.choithani@hnrtech.com>
2022-09-19 22:19:58 +0530

    deployed

--- includes/config.php
+++ includes/config.php
@@ -4,6 +4,7 @@ ob_start(); // waits until all code is loaded
 date_default_timezone_set("Europe/London");
 
 try {
+    // change this for deployment and pword through env in bash.rc file
     $con = new PDO("mysql:dbname=youtube_clone;host=localhost", "root", getenv('MYSQL_YTC_P'));
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 } catch (PDOException $e) {
